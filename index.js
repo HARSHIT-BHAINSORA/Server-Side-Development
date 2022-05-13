9517d2fde6cf0b0e48560ed3e931f881adf17606
HARSHIT <bharshit468@gamail.com>
2022-05-13 06:38:50 +0530

    Using node modules

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 //object
-var rect ={
-    perimeter : (x,y)=>(2*(x+y)),
-    area : (x,y)=>(x*y)
-};
+var rect = require("./rectangle")
 
 function solveRect(l,b){
     console.log("computing the rectangle value " + l +"and"+b)
